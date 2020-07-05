cb0780489c78d9955a0b3df0bc0b75683a3576a1
Gubendran Lakshmanan <glakshmanan@greenlots.com>
2020-07-05 13:07:12 -0700

    testing angular

---
+++ ngwithboot/backend/src/main/java/com/example/ngwithboot/NgwithbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ngwithboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgwithbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgwithbootApplication.class, args);
+	}
+
+}
---
+++ ngwithboot/backend/src/test/java/com/example/ngwithboot/NgwithbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngwithboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgwithbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
