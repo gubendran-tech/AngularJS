d6e01050013713406d9eb558bea2c9dbfd4875c8
Gubendran Lakshmanan <glakshmanan@greenlots.com>
2020-07-05 20:14:24 -0700

    Adding my first typeScript hello world

---
+++ my-angular-test-app/src/typeScript-test/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello " + person + ", Welcome to TypeScript !!!";
+}
+var user = "Gubendran ";
+document.body.textContent = greeter(user);
---
+++ my-angular-test-app/src/typeScript-test/greeter.ts
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "Hello " + person;
+}
+
+let user = "Gubendran user";
+document.body.textContent=greeter(user);
