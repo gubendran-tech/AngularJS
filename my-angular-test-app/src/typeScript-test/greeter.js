8f397e5b56943076bcb7cbbf4f43065b12c06818
Gubendran Lakshmanan <glakshmanan@greenlots.com>
2020-07-05 22:58:12 -0700

    Adding typescript test with interface and classes

--- my-angular-test-app/src/typeScript-test/greeter.js
+++ my-angular-test-app/src/typeScript-test/greeter.js
@@ -1,5 +1,16 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleName, lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleName + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello " + person + ", Welcome to TypeScript !!!";
+    return "Hello " + person.firstName + " " + person.lastName;
 }
-var user = "Gubendran ";
+// let userObj = {firstName: "Gubendran", lastName: "Lakshmanan"};
+// document.body.textContent=greeter(userObj);
+var user = new Student("Gubendran", " Chennai ", "Laksh");
 document.body.textContent = greeter(user);
--- my-angular-test-app/src/typeScript-test/greeter.ts
+++ my-angular-test-app/src/typeScript-test/greeter.ts
@@ -1,6 +1,21 @@
-function greeter(person) {
-    return "Hello " + person;
+class Student {
+    fullName: String;
+    constructor(public firstName: String, public middleName: String, public lastName: String) {
+        this.fullName = firstName + " " + middleName + " " + lastName;
+    }
 }
 
-let user = "Gubendran user";
+interface Person {
+    firstName: String,
+    lastName: String
+}
+
+function greeter(person: Person) {
+    return "Hello " + person.firstName + " " + person.lastName;
+}
+
+let userObj = {firstName: "Gubendran", lastName: "Lakshmanan"};
+document.body.textContent=greeter(userObj);
+
+let user = new Student("Gubendran", " Chennai ", "Laksh");
 document.body.textContent=greeter(user);
